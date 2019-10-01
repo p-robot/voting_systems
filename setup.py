8708e8f0cea767cee387a46c7b2b50c70530aa76
Will Probert <will@probert.co.nz>
2019-10-01 11:38:39 +0100

    ENH: Update voting systems to correct coombs_method so it will run for the Ebola case study (using a while loop instead of a bunch of if conditions)

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import numpy as np
+
+
+DESCRIPTION = "Voting systems for decision-making with multiple epidemiological model"
+NAME = "voting_systems"
+AUTHOR = "Will Probert"
+AUTHOR_EMAIL = "william.probert@bdi.ox.ac.uk"
+MAINTAINER = "Will Probert"
+MAINTAINER_EMAIL = "william.probert@bdi.ox.ac.uk"
+URL = "https://github.com/p-robot/voting_systems"
+DOWNLOAD_URL = "https://github.com/p-robot/voting_systems"
+LICENSE = "Apache License, Version 2.0"
+import voting_systems
+VERSION = voting_systems.__version__
+
+
+setup(  
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    maintainer = MAINTAINER,
+    maintainer_email = MAINTAINER_EMAIL,
+    url = URL,
+    download_url = DOWNLOAD_URL,
+    license = LICENSE,
+    requires=['numpy','pandas'],
+    packages=["voting_systems", "voting_systems.tests", "voting_systems.data"],
+    package_data={"voting_systems": ["data/ebola_models.csv",]},
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Mathematics',
+                   'Operating System :: OS Independent'],
+    include_dirs = [np.get_include()]
+    )
