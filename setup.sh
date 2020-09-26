49f6264cd00aea4970d7113bc03412104051408d
p-robot <will@probert.co.nz>
2020-09-26 23:43:23 +0100

    Adjust setup.sh to include set up of virtual environment, installation of several modules, alling of data cleaning scripts

--- setup.sh
+++ setup.sh
@@ -6,13 +6,29 @@
 
 mkdir -p data graphics
 
+# Create and activate the virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install the required packages
+pip install numpy pandas matplotlib pytest wheel
+
+# Install the voting_systems package (developer mode)
+pip install -e .
+
 # Clone the data from the git repo
 git clone git@github.com:p-robot/objectives_matter.git objectives_matter
-cp ./objectives_matter/data/*.csv ./data/
+cp ./objectives_matter/data/*.csv data
 rm -rf objectives_matter
 
 # Clean the data on the FMD case study
-python3 clean_fmd_case_study_data.py
+python3 clean_fmd_case_study_data.py "data" "data"
 
 # Clean the data on the Ebola case study
-python3 clean_ebola_case_study_data.py
+python3 clean_ebola_case_study_data.py "data" "data"
+
+# Save module information
+pip freeze > requirements.txt
+
+# Deactivate the virtual environment
+deactivate
