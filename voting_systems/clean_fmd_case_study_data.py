ec9cfc4279754c472db7df1b8523eb7209538b9c
p-robot <will@probert.co.nz>
2020-09-26 23:38:18 +0100

    Update cleaning script for FMD data so that directories are passed as input arguments; additional files are output with action names instead of ranks

--- voting_systems/clean_fmd_case_study_data.py
+++ voting_systems/clean_fmd_case_study_data.py
@@ -2,44 +2,62 @@
 """
 Process the output from Probert et al (2016)
 
-This script pulls the data from Probert et al. (2016), converts the performance scores into ranked
-votes, and runs the vote-processing rules on the data (as defined within voting_systems.py).  
+This script does the following: 
+1. reads data from Probert et al. (2016)
+2. converts the performance scores into ranked votes
 
 W. Probert, 2020
 """
 
-import pandas as pd, numpy as np, re
+import pandas as pd, numpy as np, re, sys
 from os.path import join
 
-# Read in CSV files
-models = ["a", "b", "c", "d", "e"]
-datasets = [pd.read_csv(join("voting_systems", "data", "model_" + m + ".csv")) for m in models]
-df = pd.concat(datasets)
-
-controls = df.control.unique()
-df['livestock_culled'] = df.cattle_culled + df.sheep_culled
-objectives = ['duration', 'livestock_culled', 'cattle_culled']
-
-
-# Function = Values2Votes(df, objective_col = "duration" objective_fn = "min", id_cols, direction = "wide", control_col = None)
-# With direction "wide" the column names are the controls.  With direction "long" control names are in the column "control"
-# can save wide format, values, votes, ... 
-# EBOLA - to long format.  then use this function
-
-
-for obj in objectives:
-    df_wide = df.groupby(['model', 'run', 'control'])[obj].sum().unstack('control').reset_index()
-    print(df_wide.head())
-    df_wide["objective"] = obj
+if __name__ == "__main__":
     
-    df_wide.to_csv(join("voting_systems", "data", "fmd_data_cleaned_"+obj+".csv"), index = False)
+    # Parse command-line arguments
+    if len(sys.argv) > 1:
+        DATA_DIR = sys.argv[1]
+    else:
+        DATA_DIR = "data"
+
+    if len(sys.argv) > 2:
+        OUTPUT_DIR = sys.argv[2]
+    else: 
+        OUTPUT_DIR = "data"
     
-    # Generate votes for each action from each model
-    votes = [np.argsort(row.values[2:]) for (index, row) in df_wide.iterrows()]
-    votes = np.asarray(votes)
+    # Read in CSV files
+    models = ["a", "b", "c", "d", "e"]
+    datasets = [pd.read_csv(join(DATA_DIR, "model_" + m + ".csv")) for m in models]
+    df = pd.concat(datasets)
+
+    actions = df.control.unique()
+    df['livestock_culled'] = df.cattle_culled + df.sheep_culled
+
+    objectives = ['duration', 'livestock_culled', 'cattle_culled']
+
+    for obj in objectives:
+        df_wide = df.groupby(['model', 'run', 'control'])[obj].sum().unstack('control').reset_index()
+        df_wide["objective"] = obj
+        
+        df_wide.to_csv(join(OUTPUT_DIR, "fmd_data_cleaned_" + obj + ".csv"), index = False)
     
-    # Extract only model names
-    models = df.model.values
+        # Generate votes for each action from each model
+        votes = [np.argsort(row[actions].values) for (index, row) in df_wide.iterrows()]
+        votes = np.asarray(votes)
     
-    df_votes = pd.DataFrame(np.append(df_wide[['model', 'run']].values, votes, axis = 1))
-    df_votes.to_csv(join("voting_systems", "data", "fmd_data_votes_" + obj + ".csv"), index = False)
+        # Convert candidate indices to candidate names
+        votes_str = np.asarray([actions[v] for v in votes])
+    
+        # Extract only model names
+        models = df.model.values
+    
+        colnames = ['model', 'run'] + [f'rank{i}' for i in np.arange(1, len(actions)+1)]
+    
+        df_votes = pd.DataFrame(np.append(df_wide[['model', 'run']].values, votes, axis = 1))
+        df_votes.columns = colnames
+        df_votes.to_csv(join(OUTPUT_DIR, "fmd_data_votes_" + obj + ".csv"), index = False)
+    
+        df_votes_str = pd.DataFrame(np.append(df_wide[['model', 'run']].values, 
+            votes_str, axis = 1))
+        df_votes_str.columns = colnames
+        df_votes_str.to_csv(join(OUTPUT_DIR, "fmd_data_votes_str_" + obj + ".csv"), index = False)
