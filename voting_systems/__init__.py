b023952f2a695001a02d2c45e7abf287e3fd9c28
p-robot <will@probert.co.nz>
2020-09-26 23:41:32 +0100

    Adjust __init__.py so that functions from module are imported (side stepping the submodule core)

--- voting_systems/__init__.py
+++ voting_systems/__init__.py
@@ -1,3 +1,6 @@
 """Voting systems for decision-making with multiple models"""
 name = "voting_systems"
 __version__ = '0.1'
+
+from .core import fpp, borda_count, coombs_method, alternative_vote
+from .core import values_to_votes
