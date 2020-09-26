0ff7c491dd9ec6fe6d00510168c0892b23f3b26f
p-robot <will@probert.co.nz>
2020-09-26 23:43:51 +0100

    Update run script to use double quotes around bash variables

--- run.sh
+++ run.sh
@@ -9,10 +9,10 @@ data_dir="voting_systems/data"
 output_dir="results"
 
 # Run analysis 1
-python3 voting_systems/analysis1_case_studies.py $data_dir $output_dir
+python3 voting_systems/analysis1_case_studies.py "$data_dir" "$output_dir"
 
 
 # Run analysis 2
-python3 voting_systems/analysis2_biased_models_fmd.py $data_dir $output_dir
+python3 voting_systems/analysis2_biased_models_fmd.py "$data_dir" "$output_dir"
 
 # Run analysis 3
