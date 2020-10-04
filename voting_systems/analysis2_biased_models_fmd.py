1661878e779697c67176583e3478ce77709c8433
p-robot <will@probert.co.nz>
2020-10-05 00:39:13 +0100

    Adjust analysis 2 script to run for several input datasets, including the ebola dataset

--- voting_systems/analysis2_biased_models_fmd.py
+++ voting_systems/analysis2_biased_models_fmd.py
@@ -21,14 +21,15 @@ vote_processing_rules = [\
     voting.coombs_method, \
     voting.alternative_vote]
 
-dataset_files = [\
+datasets = [\
     "fmd_data_votes_cattle_culled.csv", \
     "fmd_data_votes_livestock_culled.csv", \
-    "fmd_data_votes_duration.csv"]
+    "fmd_data_votes_duration.csv", \
+    "ebola_data_votes_cases.csv"]
 
-N_BIASED_MODELS = 5
-CANDIDATES = np.arange(5)
-Nvotes = 100
+n_biased_models = [5, 5, 5, 37]
+candidates_list = [np.arange(5), np.arange(5), np.arange(5), np.arange(6)]
+n_votes_list = [100, 100, 100, 1]
 
 if __name__ == "__main__":
     
@@ -43,6 +44,9 @@ if __name__ == "__main__":
     else: 
         OUTPUT_DIR = "."
     
+    # Set seed for repeatable results
+    np.random.seed(2021)
+    
     # Pull today's date (for saving in output files)
     now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     
@@ -50,27 +54,27 @@ if __name__ == "__main__":
     results = []
     
     # Loop through datasets
-    for i, filename in enumerate(dataset_files):
+    for f, n_biased, cand, nvotes in zip(datasets, n_biased_models, candidates_list, n_votes_list):
         
         # Load datasets
-        df_votes = pd.read_csv(join(DATA_DIR, filename))
+        df_votes = pd.read_csv(join(DATA_DIR, f))
         
         # Add up to N_BIASED_MODELS to the current votes
-        for N in np.arange(0, N_BIASED_MODELS + 1):
+        for N in np.arange(0, n_biased + 1):
             
             # Have the new biased models vote for a single action
-            for BIASED_CANDIDATE in CANDIDATES:
+            for BIASED_CANDIDATE in cand:
                 
                 vote_cols = [c for c in df_votes.columns if "rank" in c]
                 votes = df_votes[vote_cols].to_numpy().astype(int)
                 
                 # Add an additional model with all 1st preferences for first action
                 # (randomly allocating other preferences)
-                remaining_candidates = [c for c in CANDIDATES if c != BIASED_CANDIDATE]
+                remaining_candidates = [c for c in cand if c != BIASED_CANDIDATE]
                 
                 if N > 0:
                     new_model_prefs = [[BIASED_CANDIDATE] + \
-                    list(np.random.permutation(remaining_candidates)) for i in range(Nvotes * N)]
+                    list(np.random.permutation(remaining_candidates)) for i in range(nvotes * N)]
                     
                     votes = np.vstack((votes, new_model_prefs))
                 
@@ -81,7 +85,7 @@ if __name__ == "__main__":
                     (winner, winner_index), (candidates, output) = rule(votes)
                     
                     # Save outputs in an array
-                    results.append([filename, rule.__name__, N, BIASED_CANDIDATE, winner, now])
+                    results.append([f, rule.__name__, N, BIASED_CANDIDATE, winner, now])
     
     # Coerce array to dataframe
     df_results = pd.DataFrame(results)
